86804b397e585826d0d0f3f1546e1b5b12e64753
Nookler1988 <artemselifanov23@gmail.com>
2026-04-23 18:50:57 +0300

    feat(blog): add article about AI and copyright

--- data/blog.js
+++ data/blog.js
@@ -69,6 +69,7 @@ import lokalnyeIiModeliOllama from './blog/iskusstvennyy-intellekt/lokalnye-ii-m
 import gigaChatDlyaBiznesa from './blog/iskusstvennyy-intellekt/gigachat-dlya-biznesa.js'
 import iiDlyaKodaCopilotCursor from './blog/iskusstvennyy-intellekt/ii-dlya-koda-copilot-cursor.js'
 import gpt5VsClaude from './blog/iskusstvennyy-intellekt/gpt-5-vs-claude.js'
+import iiIAvtorskoePravo from './blog/iskusstvennyy-intellekt/ii-i-avtorskoe-pravo.js'
 import chtoTakoeLichnyyBrend from './blog/lichnyy-brend/chto-takoe-lichnyy-brend.js'
 import kakSozdatLichnyyBrend from './blog/lichnyy-brend/kak-sozdat-lichnyy-brend.js'
 import upakovkaLichnogoBrenda from './blog/lichnyy-brend/upakovka-lichnogo-brenda.js'
@@ -190,6 +191,7 @@ export const articles = [
   gigaChatDlyaBiznesa,
   iiDlyaKodaCopilotCursor,
   gpt5VsClaude,
+  iiIAvtorskoePravo,
   chtoTakoeLichnyyBrend,
   kakSozdatLichnyyBrend,
   upakovkaLichnogoBrenda,
